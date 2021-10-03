e68704871128f68d54db9b70b05ea59e71db71f3
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-10-02 23:41:24 -0500

    commit tree 6fceb2f17a1c80b94066f47c1973fb451d0376cc

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.substring(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');  
+  }
+
+  static titleize(string) {
+    let str = string.replace(/([^\W_]+[^\s-]*) */g, function(text) {
+      return text.charAt(0).toUpperCase() + text.substring(1).toLowerCase();
+    });
+    console.log(str)
+    let lowers = ['The', 'A', 'An', 'But', 'Of', 'And', 'For', 'At', 'By', 'From'];
+    for (let i = 0; i < lowers.length; i++){
+      str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'), 
+      function(text) {
+        return text.toLowerCase();
+      });
+    }
+  }
+
+  }
